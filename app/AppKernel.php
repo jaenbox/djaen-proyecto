33341509186e2483ea8f03576cd92bf817bfa432
David Jaen <david.jaen.revert@gmail.com>
2016-05-18 00:39:43 +0200

    heroku_djaen_v0.6

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,12 +25,9 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-            //$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            
         }
         
-        if (in_array($this->getEnvironment(), array('prod'))) {
-        	$bundles[] = new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
-        }
         
         return $bundles;
     }
